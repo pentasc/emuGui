d7a95d86a736a6dcea9b0ae95dcf03cee32ab243
pentasc <c.pentasuglia@gmail.com>
2014-10-26 14:46:22 -0400

    Seeding main python script

---
+++ Emulator_Gui.py
@@ -0,0 +1,15 @@
+# Emulator GUI for Raspberry PI
+# Corey Pentasuglia
+# 10/25/2014
+
+from tkinter import *
+
+# Generate the GUI window
+
+root = Tk()
+
+root.title("Emulator Launcher")
+root.geometry("800x700")
+
+root.mainloop()
+
